163adef8654a3f3924ea21998fef26ce3f759445
davidbosc <37219571+davidbosc@users.noreply.github.com>
2019-11-12 20:43:21 -0600

    2019-11-12
    Project compiling and functions being called correctly.  Still an issue with cudaMemcpy'ing data for the template host code

--- DeTopS_FCNs_toAdd.cu
+++ DeTopS_FCNs_toAdd.cu
@@ -31,7 +31,7 @@ unsigned TILE_WIDTH;          //Tile width of intersectKernel
 
 using namespace std;
 
-
+//instead of pointers to pointers, try pointers to arrays?
 template<typename T>
 using metric_t = T(*) (T*, T*, T*, unsigned, unsigned, unsigned, unsigned, float, unsigned);
 
@@ -48,8 +48,16 @@ __device__ T desc_jaccard_dist(
 	unsigned VECTOR_SIZE
 ) {
 
-	float descriptiveIntersectionCardinality = desc_intersection[index_B * size_A + index_A];	//I think size_A should be the subscript of the family within As
-	float unionCardinality = 0;
+	float descriptiveIntersectionCardinality = 0.0f; 
+	
+	//starting at index_B * size_A + index_A of the array containing all descriptive intersections (in row major layout), get all the vectors that aren't minFloat
+	int desc_intersections_index = index_B * size_A + index_A;
+	for (int i = 0; i < size_A; i += VECTOR_SIZE) { 	//I think size_A should be the subscript of the family within As
+		if (desc_intersection[desc_intersections_index + i] != minFloat) {
+			descriptiveIntersectionCardinality += 1.0f;
+		}
+	}
+	float unionCardinality = 0.0f;
 
 	//get the number of vectors in the description of A
 	for (int i = 0; i < size_A; i += VECTOR_SIZE) {
@@ -57,13 +65,6 @@ __device__ T desc_jaccard_dist(
 			unionCardinality += 1.0f;
 		}
 	}
-
-	//get the size of the description of B
-	/*for (int i = 0; i < size_B; i++) {
-		if (B_desc[i] != minFloat) {
-			unionCardinality++;
-		}
-	}*/
 
 	//get the number of vectors in the description of B, not in A
 	for (int i = 0; i < size_B; i += VECTOR_SIZE) {
@@ -83,18 +84,19 @@ __device__ T desc_jaccard_dist(
 			}
 		}	
 	}
-	return (1.0f - descriptiveIntersectionCardinality / unionCardinality);
+	return desc_intersection[desc_intersections_index];
+	//return (1.0f - descriptiveIntersectionCardinality / unionCardinality);
 }
 
 template <typename T>
 __device__ metric_t<T> p_desc_jaccard_dist = desc_jaccard_dist<T>;
 
 template <typename T>
-__global__ void kernel(metric_t<T> metric, T** d_A, T** d_B, T** d_inter, T* result, float minFloat, unsigned VECTOR_SIZE, unsigned VECTORS_PER_SUBSET)
+__global__ void runMetricOnGPU(metric_t<T> metric, T* d_A, T* d_B, T* d_inter, T* result, float minFloat, unsigned VECTOR_SIZE, unsigned VECTORS_PER_SUBSET)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int size = VECTOR_SIZE * VECTORS_PER_SUBSET;
-	*result = (*metric)(*d_A, *d_B, *d_inter, i, i, size, size, minFloat, VECTOR_SIZE);
+	*result = (*metric)(d_A, d_B, d_inter, i, i, size, size, minFloat, VECTOR_SIZE);
 }
 
 template <typename T>
@@ -102,19 +104,20 @@ void dIteratedPseudometric(T* A_desc, T* B_desc, T* desc_intersection, unsigned 
 
 	metric_t<T> h_desc_jaccard_dist;
 
-	T **d_A;
-	T **d_B;
-	T **d_inter;
+	T *d_A;
+	T *d_B;
+	T *d_inter;
 	
 	//desc_intersection will be calculated in a kernel in the future rather than a param
-	/**d_A = new T[size];
-	*d_B = new T[size];
-	*d_inter = new T[size];*/
+	
+	for (unsigned i = 0; i < size; i++) {
+		cout << "desc_int[" << i << "]=" << desc_intersection[i] << endl;
+	}
 
-	cudaMalloc(&A_desc, sizeof(T) * size);
-	cudaMalloc(&B_desc, sizeof(T) * size);
-	cudaMalloc(&desc_intersection, sizeof(T) * size);
-	cudaMemcpy(d_A, &A_desc, sizeof(T) * size, cudaMemcpyHostToDevice);
+	cudaMalloc((void**)&d_A, sizeof(T) * size);
+	cudaMalloc((void**)&d_B, sizeof(T) * size);
+	cudaMalloc((void**)&d_inter, sizeof(T) * size);
+	cudaMemcpy(d_A, &A_desc, sizeof(T), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_B, &B_desc, sizeof(T) * size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_inter, &desc_intersection, sizeof(T) * size, cudaMemcpyHostToDevice);
 
@@ -126,10 +129,10 @@ void dIteratedPseudometric(T* A_desc, T* B_desc, T* desc_intersection, unsigned 
 	// Copy device function pointer to host side
 	cudaMemcpyFromSymbol(&h_desc_jaccard_dist, p_desc_jaccard_dist<T>, sizeof(metric_t<T>));
 
-	kernel<T> << <1, 1 >> > (h_desc_jaccard_dist, d_A, d_B, d_inter, d_result, minFloat, VECTOR_SIZE, VECTORS_PER_SUBSET);
+	runMetricOnGPU<T> << <1, 1 >> > (h_desc_jaccard_dist, d_A, d_B, d_inter, d_result, minFloat, VECTOR_SIZE, VECTORS_PER_SUBSET);
 	cudaDeviceSynchronize();
 	cudaMemcpy(h_result, d_result, sizeof(T), cudaMemcpyDeviceToHost);
-	std::cout << "d Iterated Pseudometric Distance: " << result << std::endl;
+	std::cout << "d Iterated Pseudometric Distance: " << result << " (Should be 0.5)" << std::endl;
 }
 
 void initNegative(float* data, unsigned size) {
@@ -143,49 +146,56 @@ void setDifference(float* a, float* b, float* out) {
 	//TODO
 }
 
-void createSetDescription(float* v, float* w) {
+void getSetDescription(float* input, float* output, unsigned size) {
+	
+	//initialize the description as the minFloat
+	initNegative(output, size);
 
-	for (int fa = 0; fa < F_SUBSET_COUNT; ++fa) {
-		unsigned setIndex = (fa + 1) * VECTORS_PER_SUBSET;
-		int uniqueCount = 0;//Running total of the number of unique objects encountered
+	//The first vector in input is trivially added to output
+	for (unsigned i = 0; input[i] != minFloat && i < VECTOR_SIZE; i++) {
+		output[i] = input[i];
+	}
 
-		//For each vector in A
-		for (unsigned i = 0; i < VECTORS_PER_SUBSET; ++i) {
-			//Tracks if the current vector of v is unique (has not matched with any vectors of w)
-			bool isUnique = true;
-			//For each vector in D(A)
-			for (unsigned j = 0; j < uniqueCount; ++j) {
-				//Tracks if the current vector of v matches with the current vector of w
-				bool unique = false;
-				//For each element in current vector
+	bool inputRemaining = true;
+	unsigned outputIndex = VECTOR_SIZE;
+	//For each vector in input after the first, check 
+	for (unsigned i = 1; inputRemaining && i < size / VECTOR_SIZE; i++) {
+		bool isUnique = true;
+		if (input[i*VECTOR_SIZE] == minFloat) {
+			inputRemaining = false;
+		}
+		if (inputRemaining) {
+			//check output array for repeated vector
+			for (unsigned j = 0; j < outputIndex; j += VECTOR_SIZE) {
+				bool isVectorPartiallyIdentical = true;
 				for (unsigned k = 0; k < VECTOR_SIZE; k++) {
-					//If any two elements don't match, then the two vectors don't match
-					if (v[(fa * VECTORS_PER_SUBSET) + (k * VECTORS_PER_SUBSET * F_SUBSET_COUNT) + i]
-						!= w[setIndex + (k * WIDTH) + j]) {
-						unique = true;
-						break;
+					if (input[i*VECTOR_SIZE + k] != output[j + k]) {
+						isVectorPartiallyIdentical = false;
 					}
 				}
-
-				if (unique == false) {
+				if (isVectorPartiallyIdentical) {
 					isUnique = false;
-					//If vector is not unique, increment the conut of the vector it matched with
-					w[setIndex + (VECTOR_SIZE * WIDTH) + j]++;
-					break;
 				}
 			}
-
+			//if it is unique, add it to output and increment index
 			if (isUnique) {
-				//If the vector is unique, insert it into intersection set
-				for (int j = 0; j < VECTOR_SIZE; ++j) {
-					w[setIndex + (j * WIDTH) + uniqueCount] =
-						v[(fa * VECTORS_PER_SUBSET) + (j * VECTORS_PER_SUBSET * F_SUBSET_COUNT) + i];
+				for (unsigned m = 0; m < VECTOR_SIZE; m++) {
+					output[outputIndex + m] = input[i*VECTOR_SIZE + m];
 				}
-				w[setIndex + (VECTOR_SIZE * WIDTH) + uniqueCount] = 1;
-				uniqueCount++;
+				outputIndex += outputIndex;
 			}
 		}
 	}
+
+	////print input for debugging
+	//for (int k = 0; k < size; k++) {
+	//	cout << "in[" << k << "]= " << input[k] << endl;
+	//}
+
+	////print out the description for debugging
+	//for (int k = 0; k < size; k++) {
+	//	cout << "desc[" << k << "]= " << output[k] << endl;
+	//}
 }
 
 __global__ void descriptiveIntersectionGPU(float* desc_A_d, float* desc_B_d) {
@@ -219,28 +229,20 @@ int main(void) {
 	};
 	float b_1[6] = {
 		2,1,
-		3,2,
+		3,3,
 		4,3
 	};
 
 	unsigned size = VECTORS_PER_SUBSET * VECTOR_SIZE;
 
-	float *desc_a0 = new float[size];
-	float *desc_b0 = new float[size];
+	float* desc_a0 = new float[size];
+	float* desc_b0 = new float[size];
+	float* desc_a1 = new float[size];
+	float* desc_b1 = new float[size];
 	
 	//setup array for desc intersection kernel(?) <-- this should be done in template
 	//Hard coding intersections for now
 	float *desc_inter_h = new float[size];
-	float *desc_inter_d = new float[size];
-	/*initNegative(desc_inter_h, size);
-
-	desc_inter_h[0] = 2;
-	desc_inter_h[1] = 1;
-	desc_inter_h[2] = 3;
-	desc_inter_h[3] = 3;*/
-
-	CUDA_CHECK_RETURN(cudaMallocManaged(&desc_inter_d, sizeof(float) * size));
-
 	initNegative(desc_inter_h, size);
 
 	desc_inter_h[0] = 2;
@@ -248,23 +250,33 @@ int main(void) {
 	desc_inter_h[2] = 3;
 	desc_inter_h[3] = 3;
 
-	CUDA_CHECK_RETURN(
-		cudaMemcpy(desc_inter_d, desc_inter_h, sizeof(float) * size, cudaMemcpyHostToDevice));
+	for (unsigned i = 0; i < size; i++) {
+		cout << "dih[" << i << "]=" << desc_inter_h[i] << endl;
+	}
 
+	getSetDescription(a_0, desc_a0, size);
+	getSetDescription(b_0, desc_b0, size);
+	getSetDescription(a_1, desc_a1, size);
+	getSetDescription(b_1, desc_b1, size);
 
-	/*initNegative(desc_a0, size);
-	initNegative(desc_b0, size);*/
-	createSetDescription(a_0, desc_a0);
-	createSetDescription(b_0, desc_b0);
+	dIteratedPseudometric<float>(desc_a0, desc_b0, desc_inter_h, size);
+	
+	//cout << "done.\nFreeing memory ...";
+	//delete[] a_0;
+	//delete[] a_1;
+	//delete[] b_0;
+	//delete[] b_1;
+	//delete[] desc_a0;
+	//delete[] desc_b0;
+	//delete[] desc_inter_h;
+	//cout << "done.\nExiting program\n";
 
-
-	dIteratedPseudometric<float>(desc_a0, desc_b0, desc_inter_d, size);
 	return 0;
 }
 
 /**
  * Check the return value of the CUDA runtime API call and exit
- * the application if the call has failed.
+ * the application if the call has failed
  */
 static void CheckCudaErrorAux(const char* file, unsigned line,
 	const char* statement, cudaError_t err) {
